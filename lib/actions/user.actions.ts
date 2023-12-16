dbd6089a48fc097a7dbd55798bf14893a386545d
Ashwani Kumar Vishwakarma <ashwanikumar3811@gmail.com>
2023-12-16 17:08:44 +0530

    aded route

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { CreateUserParams } from "@/types";
+import { handleError } from "@/lib/utils";
+import { connectDb } from "@/lib/db";
+import User, { IUser } from "@/lib/db/models/user.model";
+import _ from "lodash";
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    await connectDb();
+
+    const newUser = await User.create(user);
+
+    return _.cloneDeep<IUser>(newUser);
+  } catch (err) {
+    handleError(err);
+  }
+};
