60f1f9782c7c8257ab75050964d1b44ed5777913
Ashwani Kumar Vishwakarma <ashwanikumar3811@gmail.com>
2023-12-30 12:26:49 +0530

    Update select button label in FileUploader component

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -57,7 +57,7 @@ const FileUploader = ({ onChange, imageUrl, setFiles }: FileUploaderProps) => {
           <h3 className='mb-2 mt-2'>Drag photo here</h3>
           <p className='p-medium-12 mb-4'>SVG, PNG, JPG</p>
           <Button type='button' className='rounded-full'>
-            Select from computer
+            Select from Computer
           </Button>
         </div>
       )}
